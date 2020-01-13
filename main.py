aa2cb799cea775db506163784de2e3f8539d8c17
Filip Koprivec <koprivec.filip@gmail.com>
2020-01-13 18:19:53 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from random import randint
+
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
+
+API_KEY = open("API_KEY.txt").read().strip()
+
+@app.route("/", methods=["GET"])
+def index():
+    ime_mesta = ["Ljubljana", "London", "Paris"][randint(0,2)]
+    URL = f"https://api.openweathermap.org/data/2.5/" \
+          f"weather?q={ime_mesta}&appid={API_KEY}" \
+          f"&units=metric"
+    result = requests.get(URL).json()
+    return render_template("index.html", data=result)
+
+
+if __name__ == '__main__':
+    app.run()
